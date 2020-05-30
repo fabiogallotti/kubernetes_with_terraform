d7a3fda94d07cfb7a5b41f6705cf5024da306358
Fabio Gallotti <fabio.gallotti.93@gmail.com>
2020-05-30 18:25:12 +0200

    Re-add commented part

--- global/s3/main.tf
+++ global/s3/main.tf
@@ -1,11 +1,11 @@
-# terraform {
-#   backend "s3" {
-#     bucket  = "terraform-kubernetes-fabiogallopolimi"
-#     region  = "us-east-1"
-#     key     = "global/s3/terraform.tfstate"
-#     encrypt = true
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket  = "terraform-kubernetes-fabiogallopolimi"
+    region  = "us-east-1"
+    key     = "global/s3/terraform.tfstate"
+    encrypt = true
+  }
+}
 
 provider "aws" {
   region = "us-east-1"
